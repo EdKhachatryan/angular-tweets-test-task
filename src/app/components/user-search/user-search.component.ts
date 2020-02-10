ad01741373535382aa4fdb0d5d860908587cf8ac
edkhachatryan <eduard.khachatrian@gmail.com>
2020-02-11 03:01:36 +0400

    user search component finishing

--- src/app/components/user-search/user-search.component.ts
+++ src/app/components/user-search/user-search.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {DataModel} from '../../models/data.model';
+import {FormControl} from '@angular/forms';
+import {DataWrapperService} from '../../services/data-wrapper.service';
+import {SearchService} from '../../services/search.service';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-user-search',
@@ -7,7 +13,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSearchComponent implements OnInit {
 
-  constructor() { }
+
+
+  public users$: Observable<DataModel[]>;
+  public datacource$: Observable<DataModel[]>;
+  public filter: FormControl;
+
+
+  constructor(private wrapperService: DataWrapperService,
+              private searchService: SearchService) {
+
+    this.users$ = this.wrapperService.getData(environment.api.users).map(res => res.results);
+    this.filter = new FormControl('');
+    this.datacource$ = this.searchService.filtertable(this.users$, this.filter);
+
+  }
 
   ngOnInit() {
   }
