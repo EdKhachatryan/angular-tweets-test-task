fba6471559ec6460981961d5738e5b3b6da2fc05
edkhachatryan <eduard.khachatrian@gmail.com>
2020-02-11 03:02:59 +0400

    prod env editing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    base: 'https://anymind-recruitment-python-backend.adasia.biz/',
+    hashtags: 'hashtags/Python?offset=0',
+    users: 'users/anymindgroup?offset=0'
+  },
 };
